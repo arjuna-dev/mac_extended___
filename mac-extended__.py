ffe03e1241aa17a02df96ef2ce9e61748f44be5f
jandrocamus <arjuna1@protonmail.com>
2019-12-01 13:42:54 +0100

    Implemented using alt key instead of cmd. Implemented all pynput functions instead of osascripts

--- mac-extended__.py
+++ mac-extended__.py
@@ -6,20 +6,19 @@ from pynput.keyboard import Key, Controller
 
 kybrd = Controller()
 
-cmd_pressed = False
+alt_pressed = False
 
 def on_press(key):
     if key == keyboard.Key.alt:
-        global cmd_pressed
-        cmd_pressed = True
-        print("cmd presed")
+        global alt_pressed
+        alt_pressed = True
+        print("alt presed")
 
 def on_release(key):
     if key == keyboard.Key.alt:
-        global cmd_pressed
-        cmd_pressed = False
-        print("cmd not presed")
-
+        global alt_pressed
+        alt_pressed = False
+        print("alt not presed")
 
 def zoom_in ():
     kybrd.press(Key.cmd)
@@ -33,6 +32,22 @@ def zoom_out ():
     kybrd.release('-')
     kybrd.release(Key.cmd)
 
+def open_app_switcher():
+    kybrd.press(Key.cmd)
+    kybrd.press(Key.tab)
+    kybrd.release(Key.tab)
+    
+def close_app_switcher():
+    kybrd.release(Key.cmd)
+
+def key_left():
+    kybrd.press(Key.left)
+    kybrd.release(Key.left)
+
+def key_right():
+    kybrd.press(Key.right)
+    kybrd.release(Key.right)
+
 # # Collect events until released
 # with keyboard.Listener(
 #         on_press=on_press,
@@ -40,41 +55,13 @@ def zoom_out ():
 #     listener.join()
 
 # ...or, in a non-blocking fashion:
+
 listener = keyboard.Listener(
     on_press=on_press,
     on_release=on_release)
 listener.start()
 
-
 Arduino_Serial = serial.Serial('/dev/tty.usbmodem14201',9600)    
-
-open_app_switcher    = '''tell application "System Events"
-                            delay 0.1
-                            key down command
-                            keystroke tab
-                            delay 1
-                        end tell'''
-
-close_app_switcher   = '''tell application "System Events"
-                            key up command
-                        end tell'''
-
-key_left             = '''tell application "System Events"
-                            key code 123
-                        end tell'''
-
-key_right            = '''tell application "System Events"
-                            key code 124
-                        end tell'''
-
-# zoom_in              = '''tell application "System Events"
-#                             key code 24 using {command down}
-#                         end'''
-
-# zoom_out             = '''tell application "System Events"
-#                             key code 27 using {command down}
-#                         end'''
-
 
 touches              = 0
 notouches            = 0
@@ -82,31 +69,35 @@ old_distance         = 0
 
 while True:
     arduino_data     = Arduino_Serial.readline()
-    print(cmd_pressed)
+    print(alt_pressed)
     if arduino_data == b'touch\r\n':
         touches += 1
         if touches>10:
             print("touch alert!!!")
-            p = Popen(['osascript', '-e', open_app_switcher])
+            open_app_switcher()
+            # p = Popen(['osascript', '-e', open_app_switcher])
             touches = 0
             while True:
                 arduino_data = Arduino_Serial.readline()
                 print('Still touching 😎')
                 if arduino_data == b'left\r\n':
-                    p = Popen(['osascript', '-e', key_left])
+                    key_left()
+                    # p = Popen(['osascript', '-e', key_left])
                 if arduino_data == b'right\r\n':
-                    p = Popen(['osascript', '-e', key_right])
+                    key_right()
+                    # p = Popen(['osascript', '-e', key_right])
                 if arduino_data == b'notouch\r\n':
                     notouches += 1
                 if notouches > 10:
-                    p = Popen(['osascript', '-e', close_app_switcher])
+                    close_app_switcher()
+                    # p = Popen(['osascript', '-e', close_app_switcher])
                     notouches = 0
                     break
-    elif cmd_pressed == True and arduino_data == b'You moved closer\r\n':
+    elif alt_pressed == True and arduino_data == b'You moved closer\r\n':
         print('You moved closer')
         zoom_out()
         # p = Popen(['osascript', '-e', zoom_out])
-    elif cmd_pressed == True and arduino_data == b'You moved away\r\n':
+    elif alt_pressed == True and arduino_data == b'You moved away\r\n':
         print('You moved away')
         zoom_in()
         # p = Popen(['osascript', '-e', zoom_in])
